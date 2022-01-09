62dcbed48260c63ee73b779b11fe9e9c4c3f041a
Numenot <qlv6719@gmail.com>
2022-01-09 11:32:54 +0100

    installed axios and connected heroku api for movies

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -7,34 +9,21 @@ class MainView extends React.Component {
   constructor() {
     super();
     this.state = {
-      movies: [
-        {
-          _id: 1,
-          Title: 'Pulp Fiction',
-          Description: 'In the realm of underworld, a series of incidents intertwines the lives of two Los Angeles mobsters, a gangster\'s wife, a boxer and two small-time criminals.',
-          ImagePath: 'https://upload.wikimedia.org/wikipedia/en/3/3b/Pulp_Fiction_%281994%29_poster.jpg',
-          Genre: 'Crime/Drama',
-          Director: 'Quentin Tarantino'
-        },
-        {
-          _id: 2,
-          Title: 'The Thing',
-          Description: 'A research team finds an alien being that has fallen from the sky and is starting to hunt them down. Things take a sinister turn when they realise that the creature can take the shape of its victims.',
-          ImagePath: 'https://m.media-amazon.com/images/I/91U8fI0EBdL._SY445_.jpg',
-          Genre: 'Horror',
-          Director: 'John Carpenter'
-        },
-        {
-          _id: 3,
-          Title: 'Big Trouble in Little China',
-          Description: 'Jack Burton, a truck driver, gets dragged into the mysterious underworld beneath Chinatown where he faces an ancient sorcerer named Lo Pan.',
-          ImagePath: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLmoXApny7AwjOTn3LV9vbfrvb0KTSsxVYtfEE5-y1qWvOiPsi',
-          Genre: 'Action',
-          Director: 'John Carpenter'
-        }
-      ],
+      movies: [],
       selectedMovie: null
     }
+  }
+
+  componentDidMount() {
+    axios.get('https://myflixmovieapp-myflix.herokuapp.com/movies')
+      .then(response => {
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   setSelectedMovie(newSelectedMovie) {
